bd1e39c039e6d4b46afd577a1259b38214196cc6
Valentisa11 <valya.kobylina@yandex.ru>
2022-11-05 19:12:39 +0500

    Добавьте файлы проекта.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Jane";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
