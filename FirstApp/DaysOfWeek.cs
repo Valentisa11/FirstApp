6120d5bcdb3b5a059f09122787b1c40193b51ce9
Valentisa11 <valya.kobylina@yandex.ru>
2022-11-08 21:14:38 +0500

    Add DaysOfWeek.cs

---
+++ FirstApp/DaysOfWeek.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstApp
+{
+	enum FirstApp.DaysOfWeek : byte
+	{
+		Monday = 1,
+		Tuesday,
+		Wednesday,
+		Thursday,
+		Friday,
+		Saturday,
+		Sunday
+	}
+}
--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,19 +2,36 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        class MainClass
         {
-            string MyName = "Jane";
-            byte MyAge = 27;
-            bool HaveIApet = true;
-            double MyShoeSize = 37.5;
+            enum DaysOfWeek : byte
 
-            Console.WriteLine(MyName);
-            Console.WriteLine("My name is " + MyName);
-            Console.WriteLine("My age is " + MyAge);
-            Console.WriteLine("Do I have a pet? " + HaveIApet);
-            Console.WriteLine("My shoe size is " + MyShoeSize); 
-            Console.ReadKey();
+            {
+                Monday = 1,
+                Tuesday,
+                Wednesday,
+                Thursday,
+                Friday,
+                Saturday,
+                Sunday,
+            }
+            public static void Main(string[] args)
+            {
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter your age: ");
+                byte age = checked((byte)int.Parse(Console.ReadLine()));
+                Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+                Console.WriteLine("What is your favorite day of week? ");
+                DaysOfWeek favday = (DaysOfWeek) int.Parse(Console.ReadLine());
+                Console.WriteLine("Your favorite day is {0} ", favday);
+                Console.ReadKey();
+                }
+
+            
+
         }
     }
 }
+        
+    
